5db4c491fe773afa33f8d402dc2cd389289914ea
devv64 <110506455+devv64@users.noreply.github.com>
2022-10-30 01:12:16 -0400

    Update Author.jsx

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import Image from 'next/image';
 
-const Author = ({ author }) => {
-  return (
+const Author = ({ author }) => (
     <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
         <div className='absolute left-0 right-0 -top-14'>
+          <div class="container mx-auto content-center">
             <Image
                 alt={author.name}
                 unoptimized
                 height="100"
-                width="100"
-                className="align-middle rounded-full"
+                width="150"
                 src={author.photo.url}
+                className="content-center rounded-full"
             />
+          </div>
         </div>
         <h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
         <p className='text-white text-lg'>{author.bio}</p>
     </div>
-  )
-}
+  );
+
 
 export default Author
